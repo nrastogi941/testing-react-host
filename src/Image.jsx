64c851ea0d8ce16fa8e7110b919222ca348fd368
NimitGit <nrastogi941@gmail.com>
2021-09-10 13:12:33 +0530

    tested git status

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-const Image = (props) =>
-{
-    return  <img src={props.imgsrc} alt="phots not available"  className="card_img" />;
+const Image = props => {
+    return 
+        <img 
+            src={props.imgsrc} 
+            alt="phots not available" 
+            className="card_img"     
+        />;
 };
 
 export default Image;
